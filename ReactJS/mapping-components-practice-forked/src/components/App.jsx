82b6073e14b63bf8f432c40087bbf2e75cd43536
Jing <saintshintina@gmail.com>
2022-09-26 22:42:58 +0300

    mapping components exercise

---
+++ ReactJS/mapping-components-practice-forked/src/components/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+// 1. create Entry component
+// 2. props to replace hardcoded data
+// 3. import the emojipedia const
+// 4. map through the emojipedia array and render Entry components
+
+//Emojipedia has 3 entries, so createEntry gets called 3 times.
+//Each time, it passes 1 item from the emojipedia array as a var called emojiTerm.
+
+//var emojiTerm = {
+//   id: 1,
+//   emoji: "💪",
+//   name: "Tense Biceps",
+//   meaning:
+//     "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+// }
+
+function CreateEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(CreateEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
